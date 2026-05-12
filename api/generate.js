67bb71e4b8f40a446ab113e279c9957f8dbd5bab
dipabalimisra <dipabali.misra@codeclouds.in>
2026-05-12 14:24:32 +0530

    Fix ERR_REQUIRE_ESM by adding type module and using dynamic import in api handler

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,7 @@
-import app from "../server/app.js";
+const appPromise = import("../server/app.js");
 
-export default app;
+export default async function handler(req, res) {
+  const { default: app } = await appPromise;
+  // If app is an Express instance, it can handle (req, res)
+  return app(req, res);
+}
